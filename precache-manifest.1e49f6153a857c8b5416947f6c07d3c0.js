e9a6e5eab7f754e43c21048a809434189c4b0bd5
Kunal Pant <kunalpant@Kunals-MacBook-Air.local>
2020-07-16 17:48:40 +0530

    Updates

---
+++ precache-manifest.1e49f6153a857c8b5416947f6c07d3c0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41426e5b663d949af8dfc8d92264f994",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "1a765357c6d78ea6f098",
+    "url": "/me/static/css/main.7bd5d240.chunk.css"
+  },
+  {
+    "revision": "3bb88b8e03538f5d3ec9",
+    "url": "/me/static/js/2.ec64f56c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/me/static/js/2.ec64f56c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a765357c6d78ea6f098",
+    "url": "/me/static/js/main.64a1ce83.chunk.js"
+  },
+  {
+    "revision": "cfb4880357d54ca06dcd",
+    "url": "/me/static/js/runtime-main.3ab2f894.js"
+  },
+  {
+    "revision": "50ad59929d4c40a0b97c236cbe9b3056",
+    "url": "/me/static/media/background1.50ad5992.jpg"
+  },
+  {
+    "revision": "9f6fbe39569d8297b2a38d09ffe10835",
+    "url": "/me/static/media/background2.9f6fbe39.jpg"
+  },
+  {
+    "revision": "5deaaa13b724c4dbcd4cd2adb73c0baa",
+    "url": "/me/static/media/background3.5deaaa13.jpg"
+  },
+  {
+    "revision": "e2121c204ea0a271ac71c0dd5911f6d3",
+    "url": "/me/static/media/kunal.e2121c20.jfif"
+  }
+]);
